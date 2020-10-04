de92d5ae3f00071e010847d575873eaf14e8bf16
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2020-10-04 15:07:44 -0300

    Allow all origins on server CORS

--- backend/server/server.go
+++ backend/server/server.go
@@ -15,6 +15,20 @@ type Server struct {
 
 func (s *Server) Init() {
 	s.Server = gin.Default()
+	// Dilable CORS
+	s.Server.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
 
 	routes := s.Server.Group("/api")
 	{
